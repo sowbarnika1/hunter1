3319a66c375fcfea4e1bc2cddfd69b719ef9aa1f
sowbarnika1 <46050816+sowbarnika1@users.noreply.github.com>
2019-04-01 15:28:39 +0530

    Create set11.c

---
+++ set11.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int main()
+{
+    int n,a[100000],i,j,f=0;;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        for(j=i+1;j<n;j++)
+        {
+            if(a[i]==a[j])
+            {
+                a[j]='\0';
+                if(a[i]!='\0')
+                {
+                printf("%d ",a[i]);
+                }
+                f++;
+            }
+        }
+    }
+    if(f==0)
+    {
+        printf("unique");
+    }
+    return 0;
+}
